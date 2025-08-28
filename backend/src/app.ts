420c95c3e1994b534262a127f8aabf7f328b6ade
nishantbhavasar <nishantbhavasar@gmail.com>
2025-08-28 22:15:13 +0530

    feat(Docker) : Dockerise application for multiplatform support

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,8 +13,8 @@ const app: Application = express();
 // middleware
 app.use(express.json({limit:'5mb'}));
 app.use(express.urlencoded({extended:true}));
+app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(cors({origin:"*"}));
-app.use(helmet());
 app.use(morgan("dev"));
 
 // routes
